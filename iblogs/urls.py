9660afb5dbea02a800e13c26f79e0c94fecfb5dd
osman-goni-cse <osmancse32@gmail.com>
2022-07-04 00:25:11 +0600

    admin interface changed

--- iblogs/urls.py
+++ iblogs/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+admin.site.site_header = "Osman's Planet"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
