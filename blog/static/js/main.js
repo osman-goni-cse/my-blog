45d7338db90bd001061b3f9e18d29e7bfe6225b8
osman-goni-cse <osmancse32@gmail.com>
2022-07-04 23:09:40 +0600

    footer year is now dynamic

--- blog/static/js/main.js
+++ blog/static/js/main.js
@@ -3,8 +3,11 @@ window.onscroll = function() {myFunction()};
 
 function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  console.log('Win Scroll '+winScroll);
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  console.log('Height '+height);
   var scrolled = (winScroll / height) * 100;
+  console.log('Scrolled '+scrolled);
   document.getElementById("myBar").style.width = scrolled + "%";
   
 }
